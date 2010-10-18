7ad7977aadc49546d5d2e5d1f395b0feeb43e391
kiy0taka <kiy0taka333+github@gmail.com>
2010-10-18 11:30:32 +0900

    first commit

---
+++ sample-app/src/main/java/org/kiy0taka/samples/App.java
@@ -0,0 +1,13 @@
+package org.kiy0taka.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
