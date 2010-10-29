ca580552df772cccdfcc71c38a29f03791c3c097
kiy0taka <kiy0taka333+github@gmail.com>
2010-10-29 14:30:25 +0900

    multi module findbugs settigs

---
+++ apps/sample-app/src/main/java/org/kiy0taka/samples/App.java
@@ -0,0 +1,14 @@
+package org.kiy0taka.samples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        boolean b;
+        if (b = true) {
+            System.out.println("Hello World!");
+        }
+    }
+}
---
+++ apps/sample-app/src/test/java/org/kiy0taka/samples/AppTest.java
@@ -0,0 +1,18 @@
+package org.kiy0taka.samples;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
